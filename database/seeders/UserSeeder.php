411c8061265dc8f7ceca0c7a9f5de37cfee86396
Zahra Nasfah Khaeruman <zahranasfahkh@gmail.com>
2024-09-11 09:23:39 +0700

    memodifikasi tampilan login

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        user::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
